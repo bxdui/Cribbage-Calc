7b70196c164ecfb652896a4fe9a608c959e187df
Steven <68931095+bxdui@users.noreply.github.com>
2021-04-26 14:26:35 -0400

    Re-structured 'ccwork.py' and added .txt files to outline future additions

--- ccwork.py
+++ ccwork.py
@@ -2,41 +2,42 @@ import random
 import itertools
 from collections import deque
 
-# Class defined to count paits, currently working on flushes
-
+# Class defined to count fifteens, currently working on flushes, pairs, and flipup card (start card)
 class CribbageCalc:
     def calculate():
-        # A list of single-character suits and values will be combined into a list to create a 52-card deck
-        suits = ['C', 'S', 'H', 'D']
-        values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        def gen_hand():
+            # A list of single-character suits and values will be combined into a list to create a 52-card deck
+            suits = ['C', 'S', 'H', 'D']
+            values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-        # Construct nested list of [hand, value]: v to track number values and s to track suits 
-        deck = [[v, s] for s in suits for v in values]
+            # Construct nested list of [hand, value]: v to track number values and s to track suits 
+            deck = [[v, s] for s in suits for v in values]
 
-        # quantity defines how many cards are to be placed in the hand
-        quantity = 4
+            # quantity defines how many cards are to be placed in the hand
+            quantity = 4
 
-        # Define hand as a list to represent the hand of cards
-        hand = []
+            # Define hand as a list to represent the hand of cards
+            hand = []
 
-        # Define hand_len as a check to match with quantity, ensuring the loop will continue until the number of
-        # cards in the hand equals the quantity variable. hand_len will be defined outside of the loop as an integer
-        hand_len = len(hand)
-        
-        while hand_len < quantity:
-            card = deck[random.randint(0,51)]
-            # Ensure the chosen card is not a duplicate and add it to the hand
-            if card not in hand:
-                hand.append(card)
-                # hand_len is updated on each loop to ensure proper loop count
-                hand_len = len(hand)
+            # Define hand_len as a check to match with quantity, ensuring the loop will continue until the number of
+            # cards in the hand equals the quantity variable. hand_len will be defined outside of the loop as an integer
+            hand_len = len(hand)
 
-        print(hand)
+            while hand_len < quantity:
+                card = deck[random.randint(0,51)]
+                # Ensure the chosen card is not a duplicate and add it to the hand
+                if card not in hand:
+                    hand.append(card)
+                    # hand_len is updated on each loop to ensure proper loop count
+                    hand_len = len(hand)
 
-        # Ask the user to count the points in their hand
-        verify = input("Count hand? Y or N: ")
-        
-        if verify == 'Y' or verify == 'y':
+            print(hand)
+            # Ask the user to count the points in their hand
+            verify = input("Count hand? Y or N: ")
+            if verify == 'Y' or verify == 'y':
+                count(verify, hand)
+
+        def count(verify, hand):
             # Define score as int to track score
             score = 0
             # Define hand_val to track string values to convert to int (J, Q, K)
@@ -59,13 +60,13 @@ class CribbageCalc:
             scorecard = [fifteens for i in range(len(hand_val)) for fifteens in itertools.combinations(hand_val, i) if sum(fifteens) == 15]
 
             suits = [suit for card in hand for suit in card[1]]
-            
+
             # For every value added to scorecard (added per fifteen found), add two to the score
             for i in range(len(scorecard)):
                 score += 2
-        else:
-            print('Done')
-
-        print(score)
+            
+            # Finally, display the score
+            print(score)
+        gen_hand()
 
 CribbageCalc.calculate()
